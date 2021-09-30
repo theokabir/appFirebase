365c9c8dd93a6b49c7f99207b03c49d9f9d5c5c4
theokabir <theogame4@gmail.com>
2021-09-30 16:02:41 -0300

    consultar

---
+++ www/js/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+	apiKey: "AIzaSyBYEtyUxAgp1tSz6mI2PklyxZazqUulwbc",
+  authDomain: "appagendamento-f8991.firebaseapp.com",
+  projectId: "appagendamento-f8991",
+  storageBucket: "appagendamento-f8991.appspot.com",
+  messagingSenderId: "1092988649226",
+  appId: "1:1092988649226:web:cbf7e6466badaedbc284f3",
+  measurementId: "G-M6X6L5Y1S0"
+ };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
